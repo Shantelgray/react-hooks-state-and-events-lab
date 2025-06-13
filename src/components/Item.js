cef8fa870b97b721eccd93ce3dd30a203e98af7e
Shantel <Shantelgray24@gmail.com>
2025-06-13 15:59:42 -0400

    set the shopping list to one prop

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleClick() {
+    setIsInCart((isInCart) => !isInCart);
+  }
+
   return (
-    <li className="">
+    <li className={isInCart ? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="in-cart">Add to Cart</button>
+      <button className="add" onClick={handleClick}>
+        {isInCart ? "Remove from" : "Add To"} Cart
+      </button>
     </li>
   );
 }
